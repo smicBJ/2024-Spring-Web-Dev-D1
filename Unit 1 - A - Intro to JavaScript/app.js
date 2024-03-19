0efb7b56ab0ec0f83e27c09bda14cc5636a4a462
admin <jonathan_fernandes@bjsmicschool.com>
2024-03-19 09:11:25 +0800

    Init Repo with gitignore

---
+++ Unit 1 - A - Intro to JavaScript/app.js
@@ -0,0 +1,19 @@
+
+// console.log("Hello World!");
+
+// alert("Hello, Claire");
+
+var favoriteStudent = "Richard"
+favoriteStudent = "Chloe"
+console.log(favoriteStudent)
+
+let leastFavoriteStudent = "James"
+leastFavoriteStudent = "Henrik"
+
+const thatGuy = "Jeff"
+// thatGuy = "Jordan"
+
+console.log(typeof(5))
+console.log(typeof(5.5))
+
+let isSilly = true
